c17d3bea33c401d8d3be8c77d4f2a54230ed083d
Miroslaw Bartold <miroslaw.bartold@hrs.com>
2018-02-13 13:59:43 +0100

    Longest unified substring
    Change-Id: Idf0ba0ce4916540d3568f7734dd930bb84d77b0d

--- src/main/java/LongestSubstring.java
+++ src/main/java/LongestSubstring.java
@@ -1,3 +1,17 @@
+/**
+ * A unified substring is defined as a sequence of the same characters that are a part of some String.
+ * I.e. in the String: aaaabbcccffaaaaaaaa there are the following substrings:
+ *
+ * aaaa
+ * bb
+ * ccc
+ * f
+ * aaaaaaaa
+ *
+ *
+ * Write a method that finds an index and a length of the longest unified substring in a given String.
+ */
+
 public class LongestSubstring {
 
 
@@ -5,8 +19,29 @@ public class LongestSubstring {
         int longestSubstringIdx = -1;
         int longestSubstringLength = 0;
 
+        if (input1.length() != 0) {
 
 
+            char beforeChar = input1.charAt(0);
+            int tempLongestSubstringIndex = 0;
+            int substringLength = 1;
+
+            for (int i = 1; i < input1.length(); i++) {
+                char actualChar = input1.charAt(i);
+                if (actualChar == beforeChar) {
+                    substringLength++;
+                } else {
+                    tempLongestSubstringIndex = i;
+                    substringLength=1;
+                }
+
+                if (substringLength > longestSubstringLength) {
+                    longestSubstringIdx = tempLongestSubstringIndex;
+                    longestSubstringLength = substringLength;
+                }
+                beforeChar = actualChar;
+            }
+        }
 
         return new int[]{longestSubstringIdx,longestSubstringLength};
     }
